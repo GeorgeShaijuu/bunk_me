fd4adf330733643fab6eb35def6b81967c8d7639
RYAN <ryanajitmathew334@gmail.com>
2026-02-03 12:12:44 +0530

    color change

--- bunk_calculator.py
+++ bunk_calculator.py
@@ -4,7 +4,7 @@ from math import ceil, floor
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("dark-blue")
 
-NEON_CYAN = "#1CA1AF"
+NEON_CYAN = "#1CA1AG"
 NEON_PINK = "#FF4081"
 DEEP_BG = "#121212"                     
 CARD_BG = "#212020" 
